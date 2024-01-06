c618c1f28b0cdba7b6f6c55ae67b6d3efba2f74c
DoMyLan <mylan060401@gmail.com>
2024-01-07 00:28:18 +0700

    fixed tabview UI

--- client/lib/screens/adoption_screen.dart
+++ client/lib/screens/adoption_screen.dart
@@ -544,7 +544,7 @@ class _AdoptionScreenState extends State<AdoptionScreen> {
                                   Flexible(
                                     child: Text(
                                       // '  $distanceString KM',
-                                      'KM',
+                                      '     KM',
                                     
                                       style: TextStyle(
                                         fontSize: 18,
